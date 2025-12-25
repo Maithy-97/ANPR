fb457974ef934ad5008fdbc16050e0bd9129d860
Maithili Chalke <149761590+Maithy-97@users.noreply.github.com>
2025-12-25 15:46:36 +0530

    Refactor detect_plate function and clean up code

--- app.py
+++ app.py
@@ -5,27 +5,22 @@ import numpy as np
 import re
 from ultralytics import YOLO
 from paddleocr import PaddleOCR
-import os
 
-# ------------------ INIT APP ------------------
 app = FastAPI(title="ANPR API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # change in production
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ------------------ LOAD MODELS (ONCE) ------------------
+# Load models ONCE
 model = YOLO("best.pt")
 ocr = PaddleOCR(lang="en")
-os.makedirs("temp", exist_ok=True)
-
 
 @app.post("/detect-plate")
 async def detect_plate(file: UploadFile = File(...)):
-    # Read image from React (in-memory)
     img_bytes = await file.read()
     img_array = np.frombuffer(img_bytes, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
@@ -34,47 +29,28 @@ async def detect_plate(file: UploadFile = File(...)):
         return {"plate": None}
 
     img = cv2.resize(img, (640, 640))
-
     results = model(img)
 
-    # Final values
     best_plate = ""
 
-    # Text Extraction
     for result in results:
         for box in result.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
+            plate_img = img[y1:y2, x1:x2]
 
-            # Crop license plate
-            plate = img[y1:y2, x1:x2]
-
-            # OCR
-            ocr_result = ocr.predict(plate)
-
+            ocr_result = ocr.predict(plate_img)
             if ocr_result and ocr_result[0]:
-                texts = ocr_result[0]['rec_texts']
-                scores = ocr_result[0]['rec_scores']
+                texts = ocr_result[0]["rec_texts"]
+                scores = ocr_result[0]["rec_scores"]
+
                 for text, score in zip(texts, scores):
-                    # Clean text
-                    clean_text = re.sub(r'[^A-Z0-9]', '', text.upper())
+                    clean = re.sub(r"[^A-Z0-9]", "", text.upper())
+                    if score > 0.6 and len(clean) >= 8:
+                        best_plate = clean
 
-                    # Filter likely plate strings
-                    if score > 0.6 and len(clean_text) >= 8:
-                        best_plate= clean_text
-
-    # Post process text
-    best_plate = best_plate.replace('O', '0').replace('I', '1')
-
-    # Print the value
+    best_plate = best_plate.replace("O", "0").replace("I", "1")
     return {"plate": best_plate or None}
 
 @app.get("/")
 def root():
     return {"status": "ANPR API running"}
-
-if __name__ == "__main__":
-    import uvicorn
-    import os
-
-    port = int(os.environ.get("PORT", 8000))  # fallback to 8000 locally
-    uvicorn.run(app, host="0.0.0.0", port=port)
