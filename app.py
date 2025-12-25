c52696650a393d246015c087cfe3e9636b4a712f
Maithili Chalke <149761590+Maithy-97@users.noreply.github.com>
2025-12-25 14:51:43 +0530

    Use environment variable for server port
    Updated the application to use an environment variable for the port.

--- app.py
+++ app.py
@@ -74,4 +74,7 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))  # fallback to 8000 locally
+    uvicorn.run(app, host="0.0.0.0", port=port)
